f2184c42f5af99c60c1f0d2c2985fccf7aadb479
Christof <christof.wittig@gmail.com>
2019-09-10 17:19:15 -0700

    First refactoring (as comment in PY file)

--- bikeshare_cw.py
+++ bikeshare_cw.py
@@ -6,6 +6,8 @@ import time
 import pandas as pd
 import numpy as np
 
+# This is my first refactoring!
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
